ff3afc8d2e9024fe16445a723bfd6c3517958e77
Brian Pak <brianpak27@gmail.com>
2017-12-16 11:09:53 -0800

    promise chainging, queries work

--- database/schema.sql
+++ database/schema.sql
@@ -51,22 +51,22 @@ ALTER TABLE `drivers_vehicles` ADD FOREIGN KEY (`vehicle_id`) REFERENCES `vehicl
 ALTER TABLE `available_rides` ADD FOREIGN KEY (`driver_vehicle_id`) REFERENCES `drivers_vehicles` (`id`);
 
 
--- INSERT INTO drivers (first_name, last_name) VALUES ('Allen', 'Price');
--- INSERT INTO drivers (first_name, last_name) VALUES ('Beth', 'Johnson');
--- INSERT INTO drivers (first_name, last_name) VALUES ('Doug', 'Calhoun');
--- INSERT INTO drivers (first_name, last_name) VALUES ('Easak', 'Hong');
---
--- INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('5ABC678', 'Toyota', 'Camry', 'white', 2011);
--- INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('1ZYX234', 'BMW', 'X5', 'white', 2015);
--- INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('6RPS789', 'Ford', 'Fusion', 'red', 2009);
--- INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('3TEA480', 'Nissan', 'Altima', 'blue', 2014);
---
--- INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (1, 1);
--- INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (2, 3);
--- INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (3, 2);
--- INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (4, 4);
---
--- INSERT INTO available_rides (driver_vehicle_id, status, current_ride_id, type, latitude, longitude) VALUES (1, 1, 653, 'uber', 123.5768432, 54.8293202);
--- INSERT INTO available_rides (driver_vehicle_id, status, type) VALUES (2, 0, 'uber');
--- INSERT INTO available_rides (driver_vehicle_id, status, current_ride_id, type) VALUES (3, 1, 477, 'xl');
--- INSERT INTO available_rides (driver_vehicle_id, status, type) VALUES (4, 1, 'black');
+INSERT INTO drivers (first_name, last_name) VALUES ('Allen', 'Price');
+INSERT INTO drivers (first_name, last_name) VALUES ('Beth', 'Johnson');
+INSERT INTO drivers (first_name, last_name) VALUES ('Doug', 'Calhoun');
+INSERT INTO drivers (first_name, last_name) VALUES ('Easak', 'Hong');
+
+INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('5ABC678', 'Toyota', 'Camry', 'white', 2011);
+INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('1ZYX234', 'BMW', 'X5', 'white', 2015);
+INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('6RPS789', 'Ford', 'Fusion', 'red', 2009);
+INSERT INTO vehicles (license_plate, make, model, color, year) VALUES ('3TEA480', 'Nissan', 'Altima', 'blue', 2014);
+
+INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (1, 1);
+INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (2, 3);
+INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (3, 2);
+INSERT INTO drivers_vehicles (driver_id, vehicle_id) VALUES (4, 4);
+
+INSERT INTO available_rides (driver_vehicle_id, status, current_ride_id, type, latitude, longitude) VALUES (1, 1, 653, 'uber', 123.5768432, 54.8293202);
+INSERT INTO available_rides (driver_vehicle_id, status, type) VALUES (2, 0, 'uber');
+INSERT INTO available_rides (driver_vehicle_id, status, current_ride_id, type) VALUES (3, 1, 477, 'xl');
+INSERT INTO available_rides (driver_vehicle_id, status, type) VALUES (4, 1, 'black');
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const faker = require('faker');
 // const db = require('./dbconnection.js');
@@ -13,19 +14,90 @@ const knex = require('knex')({
   }
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // pricing service requests for current number of drivers available
 app.get('/api/v1/drivers/count', (req, res) => {
   knex.select().table('available_rides')
     .then((data) => {
-      console.log('EVIL SPIRIT', data.length);
-      res.end(data.length);
+      console.log(data);
+      res.end(JSON.stringify({count: data.length}));
     })
 
 });
 
 // booking service requests for driver data to match with riders
 app.get('/api/v1/drivers/available', (req, res) => {
+  const drivers = [];
+  const vehicles = [];
+  const driversToSend = [];
+  const vehiclesToSend = [];
+
+  knex.select().table('available_rides')
+    .then((data) => {
+      data.forEach((driver) => {
+        // drivers.push(driver);
+        console.log('1111111111111111111', driver.driver_vehicle_id);
+        knex('drivers_vehicles').where({id: driver.driver_vehicle_id})
+          .then((driverProfiles) => {
+            console.log('2222222222222222222', driverProfiles);
+            driverProfiles.forEach((driverProfile) => {
+              console.log('3333333333333333333', driverProfile.driver_id);
+               //drivers.push(driverProfile);
+              knex('drivers').where({
+                id: driverProfile.driver_id
+              })
+                .then((driverToSend) => {
+                  console.log('444444444444444444', driverToSend);
+                  drivers.push(driverToSend);
+                  console.log('5555555555555', drivers);
+                })
+            })
+          })
+          console.log('got here ^^^^^^^^^^^^^^^^^^^');
+      });
+      console.log('got to here  $$$$$$$$$$$$$$');
+      data.forEach((vehicle) => {
+        console.log('666666666666', vehicle);
+        knex('drivers_vehicles').where({id: vehicle.driver_vehicle_id})
+          .then((vehicleProfiles) => {
+            vehicleProfiles.forEach((vehicleProfile) => {
+              console.log('777777777777777777', vehicleProfile);
+              // vehicles.push(vehicleProfile);
+              knex('vehicles').where({
+                id: vehicleProfile.vehicle_id
+              })
+                .then((vehicleToSend) => {
+                  vehicles.push(vehicleToSend);
+                  console.log('88888888888888888', vehicles);
+                })
+            })
+          })
+      });
+    })
+    .then(() => {
+      drivers.forEach((arrayRecord) => {
+        let obj = arrayRecord[0];
+        driversToSend.push(obj);
+      });
+      vehicles.forEach((vehicleRecord) => {
+        let obj = vehicleRecord[0];
+        vehicleToSend.push(obj);
+      });
+      console.log('999999999999999999999', driversToSend);
+      const ObjToSend = {
+        drivers: driversToSend,
+        vehicles: vehiclesToSend
+      }
+      console.log('YEAHHHHHHHH BOYYYYYYYYYY', JSON.stringify(ObjToSend));
+      res.end(JSON.stringify(ObjToSend));
+    })
+    .catch((err) => {
+      console.err();
+      throw err;
+    })
 
 });
 
@@ -35,27 +107,65 @@ app.post('/api/v1/ride', (req, res) => {
 });
 
 // client request to cancel a ride and reset status
-app.post('/api/v1/cancel', (req, res) => {
+
+// knex('books')
+// .where('published_date', '<', 2000)
+// .update({
+//   status: 'archived',
+//   thisKeyIsSkipped: undefined
+// })
+// Outputs:
+// update `books` set `status` = 'archived' where `published_date` < 2000
+
+app.patch('/api/v1/cancel', (req, res) => {
+  let rideId = req.body.rideId;
+  let driver_vehicle_id = req.body.driver_vehicle_id;
+  knex('available_rides')
+    .where('current_ride_id', '=', rideId)
+    .update({
+      current_ride_id: null,
+      status: 0
+    })
+    .then((id) => {
+      console.log('I made it this far');
+      axios.patch('/driverInfo', {
+        ride_id: rideId,
+        driver_id: null,
+        make: null,
+        model: null,
+        color: null,
+        picture: null,
+        license: null,
+        location: null
+      })
+        .then((data) => {
+          res.end();
+        })
+    })
 
 });
 
 //  client request to end a ride and reset status
-app.post('/api/v1/ride/end', (req, res) => {
+app.patch('/api/v1/ride/end', (req, res) => {
 
-});
-
-
-// client login that changes status of driver
-app.post('/api/v1/login', (req, res) => {
 
 });
 
 
 
-// client request to reset status and end session
-app.post('/api/v1/logout', (req, res) => {
 
-});
+//
+// // client login that changes status of driver
+// app.post('/api/v1/login', (req, res) => {
+//
+// });
+//
+//
+//
+// // client request to reset status and end session
+// app.post('/api/v1/logout', (req, res) => {
+//
+// });
 
 
 
