27d7e947a39275eb80d3245738d3bdcc4e21c025
Brian Pak <brianpak27@gmail.com>
2018-07-26 14:00:27 -0700

    change comment in database setup to ignore chats database

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 
+// ignore comments for setup
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
-// and to the database "chat".
+// and to the database "chat"
 const knex = require('knex')({
   client: 'mysql',
   connection: {
